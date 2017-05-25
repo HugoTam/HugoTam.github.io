5d3688148a9debe852fde985f84539239db10fac
Hugo <hugo@okay.do>
2017-05-25 11:20:48 +0800

    demo video

--- README.md
+++
@@ -1,2 +0,0 @@
-# HugoTam.github.io
-This is my blog.(Actual it is just becoming)
--- script/mobileComponents/MeContent.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by hugotam on 16/3/29.
- */
-var MeContent = React.createClass({
-    render: function() {
-        return <div>this is ME.</div>
-    }
-
-});
