9b6dc3a711f1de98bd860dc674c982f1cef50c97
Hugo <hugo@okay.do>
2017-07-11 04:10:29 +0800

    hack the phone

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,21 @@
 jQuery(function($){
+	// 检测是否手机
+	var ua = navigator.userAgent;
+    var ipad = ua.match(/(iPad).*OS\s([\d_]+)/),
+            isIphone = !ipad && ua.match(/(iPhone\sOS)\s([\d_]+)/),
+            isAndroid = ua.match(/(Android)\s+([\d.]+)/),
+            isMobile = isIphone || isAndroid;
+        //判断
+    if(isMobile) {
+
+        console.log("phone is"+isMobile);
+        $("body").addClass("is-phone");
+    }else {
+       console.log("not phone");
+    }
+
+
+
 	var $gallery = $(".gallery");
 	$gallery.flickity({
 	  // options
@@ -12,5 +29,7 @@ jQuery(function($){
 
 	console.log("run");
 	
+
+
 
 });
